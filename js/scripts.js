9f0f395c7cb5b05db48b8e4158ae0cb0f9fa19a1
Brian Nelson and David Zhu <brian@>
2018-08-14 12:05:55 -0700

    add recursive factorial function

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,28 @@
 
-var factorial = function(a) {
+var noRecFactorial = function(a) {
   var result = 1;
   for(var i=1;i<=a;i++) {
     result *= i;
   }
   return result;
-}
+};
 
+var withRecFactorial = function(b) {
+  var result = 1;
+  for(;b!==0;b--) {
+    result *= b;
+  }
+  return result;
+};
 
 $(document).ready(function() {
 
   $("#factorial-method").submit(function(event) {
-    var toOutput = factorial($("#numInput").val());
 
+    var toOutput = noRecFactorial($("#numInput").val());
+    var otherOutput = withRecFactorial($("#numInput").val());
     $('#output').text(toOutput);
+    $('#output').append("<br>" + otherOutput);
     $("#result").show();
 
     event.preventDefault();
